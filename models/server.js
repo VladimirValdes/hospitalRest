47dcf2dc4766b63cd405df6388964bbeff0dccfd
Vladimir <mendozahum20@gmail.com>
2021-08-28 16:28:16 -0600

    spa control

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const { bdConnection }  = require('../database/config');
 
@@ -48,16 +49,26 @@ class Server {
 
         // directorio Publico
         this.app.use( express.static('public'));
+
+         
     }
 
 
     routes() {
+
         this.app.use( this.path.auth, require('../routes/auth.route'));
         this.app.use( this.path.hospitales, require('../routes/hospitals.route'));
         this.app.use( this.path.medicos, require('../routes/medicos.route'));
         this.app.use( this.path.search, require('../routes/search.route'));
         this.app.use( this.path.usuarios, require('../routes/user.route'));
         this.app.use( this.path.uploads, require('../routes/uploads.route'));
+
+
+       // Cualquier otra ruta va a caer aqui
+
+       this.app.get('*', ( req, res ) => {
+        res.sendFile( path.resolve( __dirname, '../public/index.html'));
+    });
         
     }
 
